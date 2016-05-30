e1029f9d9aefe0d20b3a96eda298c26d95243443
Sylvia <sylvia.kieha@andela.com>
2016-05-30 14:09:55 +0300

    variable scope

--- declaration.js
+++ declaration.js
@@ -1,27 +1,35 @@
-// Declaring Variables
-// using keyword 'var', can be used for both local and global variables
-var x = 42;
+// // Declaring Variables
+// // using keyword 'var', can be used for both local and global variables
+// var x = 42;
+//
+// /* simply assigning it a value, this always declares a global variable.
+//    Not recommended.
+// */
+// y = 23;
+//
+// // using keyword 'let', this can be used to declare a block scope local var
+// let z = 234;
+//
+// // Evaluating Variables
+// /* a variable declared using the var statement without an initial specified value
+//    logs 'undefined'
+// */
+// var a;
+// console.log("The value of a is " + a); // logs "The value of a is undefined"
+//
+// // attempting to access an undeclared variable results in a ReferenceError exception
+// console.log("The value of b is " + b); // throws ReferenceError exception
+//
+// /* attempting to access an identifier declared with 'let' before initialization
+//  results in ReferenceError exception
+// */
+// console.log("The value of c is " + c); // throws ReferenceError exception
+// let c = 45;
 
-/* simply assigning it a value, this always declares a global variable.
-   Not recommended.
-*/
-y = 23;
-
-// using keyword 'let', this can be used to declare a block scope local var
-let z = 234;
-
-// Evaluating Variables
-/* a variable declared using the var statement without an initial specified value
-   logs 'undefined'
-*/
-var a;
-console.log("The value of a is " + a); // logs "The value of a is undefined"
-
-// attempting to access an undeclared variable results in a ReferenceError exception
-console.log("The value of b is " + b); // throws ReferenceError exception
-
-/* attempting to access an identifier declared with 'let' before initialization
- results in ReferenceError exception
-*/
-console.log("The value of c is " + c); // throws ReferenceError exception
-let c = 45;
+// Variable Scope
+if (true) {
+  var x = 5; // logs 5
+  let y = 4; // ReferenceError: y is not defined
+}
+console.log(x);
+console.log(y);
