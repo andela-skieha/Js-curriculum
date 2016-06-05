66806de858289b6fdf82a0be91c180bf457b9f59
Sylvia <sylvia.kieha@andela.com>
2016-06-05 12:14:56 +0300

    labels

---
+++ iterations/label.js
@@ -0,0 +1,8 @@
+/* provides a statement with an identifier that lets one refer
+   to it elsewhere in the program.
+*/
+
+markLoop: // this is the label
+  while (theMark == true) {
+    doSomething();
+  }
