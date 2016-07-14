a715c152a1f011a834e51867189149a7bb100bea
Njeri Kieha <sylvia.kieha@andela.com>
2016-07-14 15:19:50 +0300

    property (class attribute)

--- prototypes/proto3.js
+++ prototypes/proto3.js
@@ -2,12 +2,18 @@
 
 // defining a class
 // In JS, the function serves as the constructor. No need to explicity define it.
-var Person = function () {
+var Person = function (firstName) {
+  // properties: variables contained in the class that every instance has.
+  this.firstName = firstName;
+
   // constructor logs a message whenever a Person is instantiated.
-  console.log('Instance created');
+  console.log('Person instantiated');
 
 }
 
 // creating a class instance (object)
-var person1 = new Person();
-var person2 = new Person();
+var person1 = new Person('Alice');
+var person2 = new Person('Bob');
+
+console.log('person1 is ' + person1.firstName); // person1 is Alice
+console.log('person2 is ' + person2.firstName); // person2 is Bob
